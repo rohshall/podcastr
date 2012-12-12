3d4b4ee0cc8133adb3011623bb2da8ee69b76c9d
Salil Wadnerkar <rohshall@gmail.com>
2012-12-12 13:15:58 +0800

    Added a feed file parsing

--- src/main/java/com/polyglot/App.java
+++ src/main/java/com/polyglot/App.java
@@ -1,18 +1,40 @@
 package com.polyglot;
 
+import java.io.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void processFeedUrl( String feedUrl )
     {
-        RSSFeedParser.parse( "http://feeds.feedburner.com/TheEpilepticGibbonPodcastMusicShow" );
+        RSSFeedParser.parse( feedUrl );
         int numPodcasts = PodcastArray.PodcastTitle.length;
         for( int i = 0; i < numPodcasts; i++ )
         {
             System.out.println( PodcastArray.PodcastTitle[i] );
+        }
+    }
+
+
+    public static void main( String[] args )
+    {
+        try {
+            FileReader fr = new FileReader( "feeds.txt" );
+            BufferedReader br = new BufferedReader( fr );
+            String line;
+            while( (line = br.readLine()) != null )
+            {
+                System.out.println( "Feeds in " + line );
+                processFeedUrl( line );
+                System.out.println();
+            }
+        } catch( FileNotFoundException e ) {
+            System.out.println( "File not found!" );
+        } catch( IOException e ) {
+            System.out.println( "File input error!" );
         }
     }
 }
