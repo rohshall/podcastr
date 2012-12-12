8987299fbca0e3455cce625194b9be9f026bab7c
Salil Wadnerkar <rohshall@gmail.com>
2012-12-12 14:11:09 +0800

    Added podcast episode download

--- src/main/java/com/polyglot/App.java
+++ src/main/java/com/polyglot/App.java
@@ -1,6 +1,7 @@
 package com.polyglot;
 
 import java.io.*;
+import java.net.URL;
 
 /**
  * Hello world!
@@ -8,23 +9,50 @@ import java.io.*;
  */
 public class App 
 {
-    public static void processFeedUrl( String feedUrl )
+    private static void downloadUrl( String mediaUrl )
+    {
+        BufferedInputStream in = null;
+        BufferedOutputStream out = null;
+        String filename = mediaUrl.substring( mediaUrl.lastIndexOf( '/' ) + 1 );
+        try {
+            in = new BufferedInputStream(new URL( mediaUrl ).openStream());
+            out = new BufferedOutputStream( new FileOutputStream( filename ) );
+            byte data[] = new byte[1024];
+            int count;
+            while((count = in.read(data,0,1024)) != -1)
+            {
+                out.write(data, 0, count);
+            }
+        } catch( IOException e ) {
+            System.out.println( "File output error!" );
+        } finally {
+            if( out != null ) {
+                try {
+                    out.close();
+                } catch( IOException e ) {
+                }
+            }
+        }
+    }
+
+    private static void processFeedUrl( String feedUrl )
     {
         RSSFeedParser.parse( feedUrl );
         int numPodcasts = PodcastArray.PodcastTitle.length;
         for( int i = 0; i < numPodcasts; i++ )
         {
             System.out.println( PodcastArray.PodcastTitle[i] );
-            System.out.println( PodcastArray.PodcastMedia[i] );
+            System.out.println( "Downloding " + PodcastArray.PodcastMedia[i] );
+            downloadUrl( PodcastArray.PodcastMedia[i] );
         }
     }
 
 
     public static void main( String[] args )
     {
+        BufferedReader br = null;
         try {
-            FileReader fr = new FileReader( "feeds.txt" );
-            BufferedReader br = new BufferedReader( fr );
+            br = new BufferedReader( new FileReader( "feeds.txt" ) );
             String line;
             while( (line = br.readLine()) != null )
             {
@@ -36,6 +64,13 @@ public class App
             System.out.println( "File not found!" );
         } catch( IOException e ) {
             System.out.println( "File input error!" );
+        } finally {
+            if( br != null ) {
+                try {
+                    br.close();
+                } catch( IOException e ) {
+                }
+            }
         }
     }
 }
